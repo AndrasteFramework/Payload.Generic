6a462b0ba2cd526aaf94aa8a19bdf13ed70391b2
MeFisto94 <MeFisto94@users.noreply.github.com>
2022-06-05 16:20:21 +0200

    Initial Commit

---
+++ GenericEntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using Andraste.Payload.VFS;
+using EasyHook;
+
+namespace Andraste.Payload.Generic
+{
+    public class GenericEntryPoint : EntryPoint
+    {
+        public GenericEntryPoint(RemoteHooking.IContext context) : base(context)
+        {
+        }
+
+        public override string FrameworkName => "Andraste Generic";
+        public override string Version => "0.1.1";
+        protected override void PreWakeup()
+        {
+            Container.RegisterManager(new BasicFileRedirectingManager());
+        }
+
+        protected override void PostWakeup()
+        {
+        }
+
+        protected override void ApplicationReady()
+        {
+        }
+    }
+}
